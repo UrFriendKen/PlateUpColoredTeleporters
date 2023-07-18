09a1ee716ae6cd6f15ebcc1f85d6ca18da5b9ec0
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-07-18 16:44:14 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,81 @@
+﻿using HarmonyLib;
+using KitchenData;
+using KitchenMods;
+using System.Reflection;
+using UnityEngine;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenColoredTeleporters
+{
+    public class Main : IModInitializer
+    {
+        public const string MOD_GUID = $"IcedMilo.PlateUp.{MOD_NAME}";
+        public const string MOD_NAME = "Colored Teleporters";
+        public const string MOD_VERSION = "0.1.0";
+
+        const int TELEPORTER_APPLIANCE_ID = 459840623;
+
+        public Main()
+        {
+            Harmony harmony = new Harmony(MOD_GUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void PostActivate(KitchenMods.Mod mod)
+        {
+
+        }
+
+        public void PreInject()
+        {
+            if (!GameData.Main.TryGet(TELEPORTER_APPLIANCE_ID, out Appliance teleporter))
+            {
+                Main.LogError("Failed to get Teleporter GDO!");
+                return;
+            }
+            if (teleporter.Prefab == null)
+            {
+                Main.LogError("Teleporter Prefab is null!");
+                return;
+            }
+            Transform teleporterPrefabContainer = teleporter.Prefab?.transform.Find("Teleporter");
+            if (teleporterPrefabContainer == null)
+            {
+                Main.LogError("Failed to find teleporter transform!");
+                return;
+            }
+
+            MeshRenderer teleporterArrowsRenderer = teleporterPrefabContainer.Find("Arrows")?.gameObject?.GetComponent<MeshRenderer>();
+            if (teleporterArrowsRenderer == null)
+            {
+                Main.LogError("Failed to get Teleporter Arrows renderer!");
+                return;
+            }
+            MeshRenderer teleporterSurfaceRenderer = teleporterPrefabContainer.Find("Surface")?.gameObject?.GetComponent<MeshRenderer>();
+            if (teleporterSurfaceRenderer == null)
+            {
+                Main.LogError("Failed to get Teleporter Surface renderer!");
+                return;
+            }
+
+            TeleporterColorView teleporterColorView = teleporter.Prefab.AddComponent<TeleporterColorView>();
+            teleporterColorView.ArrowsRenderer = teleporterArrowsRenderer;
+            teleporterColorView.SurfaceRenderer = teleporterSurfaceRenderer;
+
+            Main.LogInfo("Successfully added TeleporterColorView.");
+        }
+
+        public void PostInject()
+        {
+        }
+
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+}
---
+++ TeleporterColorView.cs
@@ -0,0 +1,98 @@
+﻿using Kitchen;
+using KitchenMods;
+using MessagePack;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace KitchenColoredTeleporters
+{
+    public class TeleporterColorView : UpdatableObjectView<TeleporterColorView.ViewData>
+    {
+        const float DEFAULT_HUE = 0.5738632f;
+        const float SURFACE_SATURATION = 0.2508833f;
+        const float SURFACE_VALUE = 0.6792f;
+        const float ARROWS_SATURATION = 0.5905837f;
+        const float ARROWS_VALUE = 0.7264f;
+
+        const float STEP_SIZE = 1f / 20f;
+        const float MULTIPLIER = 37f;
+
+        public class UpdateView : IncrementalViewSystemBase<ViewData>, IModSystem
+        {
+            EntityQuery Views;
+            protected override void Initialise()
+            {
+                base.Initialise();
+                Views = GetEntityQuery(typeof(CConveyTeleport), typeof(CLinkedView));
+            }
+
+            protected override void OnUpdate()
+            {
+                using NativeArray<CConveyTeleport> teleports = Views.ToComponentDataArray<CConveyTeleport>(Allocator.Temp);
+                using NativeArray<CLinkedView> views = Views.ToComponentDataArray<CLinkedView>(Allocator.Temp);
+
+                for (int i = 0; i < teleports.Length; i++)
+                {
+                    CConveyTeleport teleport = teleports[i];
+                    CLinkedView view = views[i];
+
+                    SendUpdate(view, new ViewData()
+                    {
+                        Index = teleport.GroupID,
+                        Hue = GetHue(teleport.GroupID)
+                    });
+                }
+            }
+
+            private float GetHue(int index)
+            {
+                return (DEFAULT_HUE + (STEP_SIZE * index * MULTIPLIER)) % 1;
+            }
+        }
+
+        [MessagePackObject(false)]
+        public class ViewData : ISpecificViewData, IViewData.ICheckForChanges<ViewData>
+        {
+            [Key(0)] public int Index;
+            [Key(1)] public float Hue;
+
+            public IUpdatableObject GetRelevantSubview(IObjectView view) => view.GetSubView<TeleporterColorView>();
+
+            public bool IsChangedFrom(ViewData check)
+            {
+                return Index != check.Index ||
+                    Hue != check.Hue;
+            }
+        }
+
+        public int GroupID = 0;
+        float Hue = DEFAULT_HUE;
+        public MeshRenderer SurfaceRenderer;
+        public MeshRenderer ArrowsRenderer;
+
+        protected override void UpdateData(ViewData data)
+        {
+            GroupID = data.Index;
+            Hue = data.Hue;
+            Color surfaceColor = Color.HSVToRGB(Hue, SURFACE_SATURATION, SURFACE_VALUE);
+            Color arrowsColor = Color.HSVToRGB(Hue, ARROWS_SATURATION, ARROWS_VALUE);
+
+            UpdateMaterialColor(SurfaceRenderer, surfaceColor);
+            UpdateMaterialColor(ArrowsRenderer, arrowsColor);
+        }
+
+        private void UpdateMaterialColor(Renderer renderer, Color color)
+        {
+            if (renderer?.material != null)
+            {
+                if (renderer.material.name != "New Material")
+                {
+                    renderer.material = new Material(renderer.material);
+                    renderer.material.name = "New Material";
+                }
+                renderer.material.SetColor("_Color0", color);
+            }
+        }
+    }
+}
